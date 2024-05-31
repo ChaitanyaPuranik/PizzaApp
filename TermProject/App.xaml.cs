f40eea18b984f679fd16a46f9abdcc5123e8089f
Chaitanya <puranikc@shernet.sheridancollege.ca>
2024-05-31 13:52:37 -0400

    Add project files.

---
+++ TermProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TermProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TermProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TermProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
