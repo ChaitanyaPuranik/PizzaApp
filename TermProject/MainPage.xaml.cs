2a297d025896b17034fcf7d178547bd9eeca216d
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-08-07 14:46:55 -0400

    Added image to MenuPage

--- TermProject/MainPage.xaml.cs
+++ TermProject/MainPage.xaml.cs
@@ -4,7 +4,7 @@ namespace TermProject
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
@@ -13,7 +13,7 @@ namespace TermProject
 
         private async void OnTapGetStarted(object sender, TappedEventArgs e)
         {
-            await Navigation.PushAsync(new TermProject.UI.HomePage());
+            await Navigation.PushAsync(new TermProject.UI.MenuPage());
         }
     }
 
--- TermProject/UI/HomePage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace TermProject.UI;
-
-public partial class HomePage : ContentPage
-{
-	public HomePage()
-	{
-		InitializeComponent();
-	}
-}
